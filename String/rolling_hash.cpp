3fb189deb910d513a5b296931ffbdc63b784bb3a
Mosmochi <90638923+say859462@users.noreply.github.com>
2022-05-13 11:19:11 +0800

    Create rolling_hash.cpp

---
+++ String/rolling_hash.cpp
@@ -0,0 +1,24 @@
+const ll P = 75577;
+const ll MOD = 998244353;
+string s;
+ll Hash[MXN];    //Hash[i] 為字串 [0,i] 的 hash值
+Hash[0]=s[0];
+ll _p[MXN];
+void build(const string& s){
+    for(int i=i; i<s.size(); i++){
+        Hash[i]=(Hash[i-1]*p%mod+s[i])%mod;
+    }
+}
+bool check(int l,int r){
+  ll rec=Hash[r]-Hash[l-1]*p[r-1+1]%mod;
+  if(rec<0)rec+=mod;
+  return rec;
+}
+int main(){
+  _p[0]=1;
+  
+  for(int i=1;i<MXN;i++){
+    _p[i]=_p[i-1]*p%mod;
+  }
+  
+}
