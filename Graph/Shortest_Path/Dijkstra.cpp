a58903fa0df0bcc6f3487c805671b2024bef7ef4
Mosmochi <90638923+say859462@users.noreply.github.com>
2022-06-08 23:25:51 +0800

    Update Dijkstra.cpp

--- Graph/Shortest_Path/Dijkstra.cpp
+++ Graph/Shortest_Path/Dijkstra.cpp
@@ -33,3 +33,21 @@ void dijkstra()
 		}
 	}
 }
+//優化版
+init();
+priority_queue<pair<ll,int>,vector<pair<ll,int>>,greater<pair<ll,int>>> pq;
+ 
+pq.push(make_pair(dis[start], start)); // 為了方便實作，用pair包起來會先比較距離大小
+ 
+while(!pq.empty()){
+    auto [d, u] = pq.top(); pq.pop();
+    if(vis[u])    continue;  // 確保每個點最多只被走過一遍
+    vis[u] = 1;
+    for(int i = 0; i < edge[u].size(); i++){  // 窮舉此點所有連到的點
+        int v = edge[u][i].to, w = edge[u][i].weight;
+        if(dis[v] > dis[u] + w){
+            dis[v] = dis[u] + w;  // 鬆弛
+            pq.push(make_pair(dis[v], v)); // 如果有更新距離，則丟進 priority_queue
+        }
+    }
+}
