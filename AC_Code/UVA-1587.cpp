f7aa8b456405e0895d92ae367bc841036c438312
Mosmochi <90638923+say859462@users.noreply.github.com>
2022-06-16 00:33:42 +0800

    Update UVA-1587.cpp

--- AC_Code/UVA-1587.cpp
+++ AC_Code/UVA-1587.cpp
@@ -18,13 +18,13 @@ int main(){
       if(w>h)swap(w,h);
       rec.push_back(make_pair(w,h));
     }
-    sort(rec.begin(),rec.end(),cmp);
+    sort(rec.begin(),rec.end(),cmp);//排序後，面由短邊從小排到大
     bool flag=0;  
     for(int i=0;i<6;i+=2){
-      if(rec[i].first!=rec[i+1].first || rec[i].second!=rec[i+1].second)flag=1;
+      if(rec[i].first!=rec[i+1].first || rec[i].second!=rec[i+1].second)flag=1;//兩相同的面邊要相同
     }
 
-    if(rec[0].first!=rec[2].first || rec[0].second!=rec[4].first || rec[2].second!=rec[4].second)flag=1;
+    if(rec[0].first!=rec[2].first || rec[0].second!=rec[4].first || rec[2].second!=rec[4].second)flag=1;//考慮最小面的短長邊要與另外兩面的短邊相同
     if(flag)cout<<"IMPOSSIBLE"<<'\n';
     else cout<<"POSSIBLE"<<'\n';
   }
