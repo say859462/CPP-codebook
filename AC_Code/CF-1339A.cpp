40041a1db117a67c501bb73d1da191736f474343
Mosmochi <90638923+say859462@users.noreply.github.com>
2022-08-13 23:10:41 +0800

    Create CF-1339A.cpp

---
+++ AC_Code/CF-1339A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    cin.tie(0);ios::sync_with_stdio(0);
+    int t;
+    cin>>t;
+    int x,n,m;
+    while(t--){
+        cin>>n;
+        cout<<n<<'\n';
+    }
+    return 0;
+}
