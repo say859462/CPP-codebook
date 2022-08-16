e5082c128433d7f31f56acfc0d46ba5710200d08
Mosmochi <90638923+say859462@users.noreply.github.com>
2022-08-16 16:49:54 +0800

    Create CF-1537B.cpp

---
+++ AC_Code/CF-1537B.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cin.tie(0);ios::sync_with_stdio(0);
+    int n,m,t,x,y;
+    cin>>t;
+    while(t--){
+        cin>>n>>m>>x>>y;
+        cout<<1<<" "<<1<<" "<<n<<" "<<m<<'\n';
+    }
+    return 0;
+}
