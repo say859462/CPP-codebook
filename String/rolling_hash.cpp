420fd34ba67ad4f679fbeaf57b78fa5b77bdf01c
Mosmochi <90638923+say859462@users.noreply.github.com>
2022-08-29 17:57:33 +0800

    Update rolling_hash.cpp

--- String/rolling_hash.cpp
+++ String/rolling_hash.cpp
@@ -10,7 +10,7 @@ void build(const string& s){
     }
 }
 bool check(int l,int r){
-  ll rec=Hash[r]-Hash[l-1]*p[r-1+1]%mod;
+  ll rec=Hash[r]-Hash[l-1]*p[r-l+1]%mod;
   if(rec<0)rec+=mod;
   return rec;
 }
