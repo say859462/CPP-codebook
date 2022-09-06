1ec8a29fd5c87a3fe47fa8e1bd1f5b7c55b908e0
Mosmochi <90638923+say859462@users.noreply.github.com>
2022-09-06 17:27:47 +0800

    Update CF-235A.cpp

--- AC_Code/CF-235A.cpp
+++ AC_Code/CF-235A.cpp
@@ -89,19 +89,26 @@ int main(){
     // freopen("input.txt","r",stdin);    // 讀 file.in 檔
     //  freopen("output.txt","w",stdout);  // 寫入 file.out 檔
     IO_FAST;
+
     cin>>n;
     if(n==1){
         cout<<1<<endl;
+        return 0;
     }
-    else if(n==2){
+    if(n==2){
         cout<<2<<endl;
+        return 0;
+    }
+    if(n==3){
+        cout<<6<<endl;
         return 0;
     }
     if(n%2){
         cout<<n*(n-1)*(n-2)<<endl;
     }
     else{
-        cout<<n*(n-1)*(n-1)<<endl;
+        if(n%3==0)cout<<(n-3)*(n-1)*(n-2)<<endl;
+        else cout<<n*(n-1)*(n-3)<<endl;
     }
     return 0;
 }
