15f38cb278cac8743c2f2d680554c017ba6bd76f
Mosmochi <90638923+say859462@users.noreply.github.com>
2022-09-10 14:13:04 +0800

    Update Preprocessor_template.cpp

--- Preprocessor_template.cpp
+++ Preprocessor_template.cpp
@@ -7,7 +7,7 @@ z133icouISI33Zfb5GSb565T30D@@UU#dAD@#E5Iotziuuzzzz,  :,zigzz,szzzyzzsiziotctzz,z
 ,z13AuzzxJI3fG5f6V6Z0XQ##Q$$NNN@NNNNNMMNNNMMM@@NNNMJS0NQQ0x,zoz,zz,,,;zzzzzzz,zu3SG5bbbA0GVf6GV33IZS
 zzzgIVL7tuJA0UXEUDXEEDEREU0XGDAV#RD80$NMMMMMNMMNNMMMMMNNAz:.zz,zx: .;,,, ;,,;zcxu3SVfVGVGAff3TS53VVf
 czcxST8ED8X0EUDUDXDXEE6T65fV5D#U@$NNNNMNMNNNMMMNMNNNMNNNMQz z7zzz .;,;:;zz,,,zov1yS3fIfSVZVf5SS3ZVXf
- ,zu36STSddATXUE06G5VbGyL0D@$N@N@NNNNNNNNNNNNNNNNNNNNMNNMMM8bR33zcg,,,;, :,zzsssc7i1uSTVTfISS3uV000U
+,zu36STSddATXUE06G5VbGyL0D@$N@N@NNNNNNNNNNNNNNNNNNNNMNNMMM8bR33zcg,,,;, :,zzsssc7i1uSTVTfISS3uV000U
 :zZS3fd8ZLb8ASVG6fSuS3RE$#NNN@@$N@NNNNNNNNNNMNMNNNNNNNMNMNMMM$6V#6Jgz,s,::;:,,zztoovuyy1CIf3Sf000ARD
 ,z3X3I1C6dG0d53ZVVV88R@NNMMMN@$N@NNNNNNMNNNNNNNMMMNMNMMNNNNN$N@$RQ##Zfyzzz,,;,,,,iy3LLxI3T3fSVZf5EUs
 ,;IRE5530X8V6ZZI6EDEU3fSTA#$MNN@NNNNNNNNNNNNNNMNMMMNMNNNNNNNN@N@NNN@Qfuizzz,,:;.;,zsLJZTGGVfI3TfU#L,
@@ -40,31 +40,51 @@ SZuJu3C33SIT3SST3ffGS5fGfZSSL3I3C3JIuJyuyugyt7o1vtcvsoc7o11y7uyuxxxIITT5ZbVdbX8X
 VUVV3ICIuLu3JLJIC3I3L3I3C3L3I3I3CSIT33J3ISI3L3JIL3I3ISL3L3LTS5fAVbVA68AdV6dE0U8#E#EEEQ#RQ@NNNNNNNNMN
 fXXXE08VAS5fZffSZTGSS3fSf3fT5fVG65VZAVdAAV0VbZVZVZAZ6V6A0bd5VV6d88UX##RE#EQQ@RQQ$$@Q@$NNMMMMMMMMMMMM
 */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
 using namespace std;
-#define IO_FAST cin.tie(0);ios::sync_with_stdio(0)
+#define IO_FAST \
+    cin.tie(0); \
+    ios::sync_with_stdio(0)
 #define ll long long
 #define LL long long
 #define ld long double
-#define mx 1e6
-#define mod 1e9
 #define EPS 1e-8
-#define cl(x) ((x<<1))
-#define cr(x) ((x<<1)+1)
-#define FZ(x) memset(x,1,sizeof(x))
-#define lowbit(x) (x&-x)
+#define cl(x) ((x << 1))
+#define cr(x) ((x << 1) + 1)
+#define FZ(x) memset(x, 0, sizeof(x))
+#define lowbit(x) (x & -x)
 #define INF 0x3f3f3f3f
 #define INFLL 0x3f3f3f3f3f3f3f3f
 #define endl '\n'
-#define ALL(a) a.begin(),a.end() 
+#define pii pair<int,int> 
+#define pll pair<ll,ll> 
+#define vi vector<int> 
+#define vl vector<ll> 
+#define MP make_pair
+#define pi acos(-1)
+#define ALL(a) a.begin(), a.end()
 #define SZ(x) ((int)x.size())
-#define asort(a) sort(a.begin(),a.end())
-#define dsort(a) sort(a.begin(),a.end(),greater<int>())
-#define pb push_back
-
-int main(){
-	IO_FAST;
-	
-	
-	return 0;
+#define asort(a) sort(a.begin(), a.end())                  //升冪排序
+#define dsort(a) sort(a.begin(), a.end(), greater<int>())  //降冪排序             //升冪排序
+#define dsortll(a) sort(a.begin(), a.end(), greater<ll>()) //降冪排序
+#define PB push_back
+const int mod=1e8;
+int gcd(int a,int b){//輾轉相除法
+    while(b){
+        int tmp=b;
+        b=a%b;
+        a=tmp;
+    }
+    return a;
+}
+ll qpow(ll base,ll exp){//快速冪
+    ll rec=1;
+    while(exp){
+        if(exp&1)rec*=base;
+        base*=base;
+        exp>>=1;
+    }
+    return rec;
 }
